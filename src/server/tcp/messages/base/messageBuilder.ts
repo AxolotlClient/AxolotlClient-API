9a5717de62950ee66d2e0e0e485b6b9753731b58
Garrett Keneau <gart@gart.sh>
2023-07-21 01:17:38 -0400

    more shit

--- src/server/tcp/messages/base/messageBuilder.ts
+++ src/server/tcp/messages/base/messageBuilder.ts
@@ -44,67 +44,73 @@ export default class MessageBuilder<Message = {}> {
 
   public uint8<T extends string>(
     name: T
-  ):MessageBuilder<
-  {
-    [K in T]: BuiltPart<T, Uint8MessagePart<false>>;
-  } & Message
-> {
+  ): MessageBuilder<
+    {
+      [K in T]: BuiltPart<T, Uint8MessagePart<false>>;
+    } & Message
+  > {
     return this.addPart(name, new Uint8MessagePart(void 0));
   }
 
   public uint16<T extends string>(
     name: T
   ): MessageBuilder<
-  {
-    [K in T]: BuiltPart<T, Uint16MessagePart<false>>;
-  } & Message> {
+    {
+      [K in T]: BuiltPart<T, Uint16MessagePart<false>>;
+    } & Message
+  > {
     return this.addPart(name, new Uint16MessagePart(void 0));
   }
 
   public uint32<T extends string>(
     name: T
-  ):  MessageBuilder<
-  {
-    [K in T]: BuiltPart<T, Uint32MessagePart<false>>;
-  } & Message> {
+  ): MessageBuilder<
+    {
+      [K in T]: BuiltPart<T, Uint32MessagePart<false>>;
+    } & Message
+  > {
     return this.addPart(name, new Uint32MessagePart(void 0));
   }
 
   public uint64<T extends string>(
     name: T
   ): MessageBuilder<
-  {
-    [K in T]: BuiltPart<T, Uint64MessagePart<false>>;
-  } & Message> {
+    {
+      [K in T]: BuiltPart<T, Uint64MessagePart<false>>;
+    } & Message
+  > {
     return this.addPart(name, new Uint64MessagePart(void 0));
   }
 
   public buffer<T extends string>(
     name: T,
     length: number
-  ):  MessageBuilder<
-  {
-    [K in T]: BuiltPart<T, BufferMessagePart<false>>;
-  } & Message> {
+  ): MessageBuilder<
+    {
+      [K in T]: BuiltPart<T, BufferMessagePart<false>>;
+    } & Message
+  > {
     return this.addPart(name, new BufferMessagePart(length, void 0));
   }
 
   public string<T extends string>(
     name: T,
     length: number
-  ):  MessageBuilder<
-  {
-    [K in T]: BuiltPart<T, StringMessagePart<false>>;
-  } & Message> {
+  ): MessageBuilder<
+    {
+      [K in T]: BuiltPart<T, StringMessagePart<false>>;
+    } & Message
+  > {
     return this.addPart(name, new BufferMessagePart(length, void 0));
   }
 
   public uuid<T extends string>(
     name: T
-  ):  MessageBuilder<
-  {
-    [K in T]: BuiltPart<T, UUIDMessagePart<false>>;
-  } & Message> {
+  ): MessageBuilder<
+    {
+      [K in T]: BuiltPart<T, UUIDMessagePart<false>>;
+    } & Message
+  > {
     return this.addPart(name, new BufferMessagePart(16, void 0));
   }
 }
@@ -118,5 +124,3 @@ const test = new MessageBuilder()
   .string("string", 16)
   .uint64("uint64")
   .build();
-
-  
