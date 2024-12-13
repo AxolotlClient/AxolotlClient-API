39edb2bfd2e532d41d5c63c060ea371a7042a236
moehreag <moehreag@gmail.com>
2024-12-13 16:48:29 +0100

    Improve performance of authentication extractor

--- src/extractors.rs
+++ src/extractors.rs
@@ -22,10 +22,11 @@ impl FromRequestParts<ApiState> for Authentication {
 			.ok_or(StatusCode::UNAUTHORIZED)?
 			.map_err(|_| StatusCode::UNAUTHORIZED)?;
 
+		let mut transaction = database.begin().await?;
 		let authorization_ref = &*authorization;
 		let uuid = {
 			let record = query!("SELECT valid, player FROM tokens WHERE token = $1", authorization_ref)
-				.fetch_one(database)
+				.fetch_one(&mut *transaction)
 				.await?;
 
 			match record.valid {
@@ -38,12 +39,14 @@ impl FromRequestParts<ApiState> for Authentication {
 		};
 
 		query!("UPDATE players SET last_online = LOCALTIMESTAMP where uuid = $1", uuid)
-			.execute(database)
+			.execute(&mut *transaction)
 			.await?;
 
 		query!("UPDATE tokens SET used = LOCALTIMESTAMP where token = $1", authorization_ref)
-			.execute(database)
+			.execute(&mut *transaction)
 			.await?;
+
+		transaction.commit().await?;
 
 		Ok(Self(uuid))
 	}
